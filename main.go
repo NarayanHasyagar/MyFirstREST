ff9e32eaf96cd1042cb333e076281de2b7471725
Narayan Hasyagar <nhasyaga@cisco.com>
2019-03-30 23:37:42 +0530

    Added API entry point in main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main;
+
+import {
+
+	"net/http"
+	
+	"os"
+	"api/handlers"
+}
+
+func main()
+{
+	//Get the PORT environment variable if it exists
+	port := os.GetEnv("PORT")
+
+
+	if port == ""
+	{
+		//Set the port to localhost 8000 port
+		port = "8000"
+	}
+
+	//Create a new router for redirecting requests to different handlers
+	router = handlers.NewRouter()
+
+	//Start the server port number and router
+	log.Fatal(http.ListenAndServe(":"+port,router))
+
+}
+
+
